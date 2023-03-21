f87998c5b37d7d9528aa7ea34f383606d3016bd1
cmilara <cristinamilara@gmail.com>
2023-03-21 17:24:32 +0100

    form process

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -1,5 +1,6 @@
 package com.example.controllers;
 
+import java.lang.ProcessBuilder.Redirect;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.example.entities.Estudiante;
 import com.example.entities.Facultad;
@@ -62,10 +64,11 @@ public class MainController {
  * Metodo que recibe los datos procedentes de los controles del formulario
  */
 @PostMapping("/altaEstudiante")
-public void altaEstudiante(){
-    
+public String altaEstudiante(){
+    return "redirect:/listar";
 
+    }
 
 }
 
-}
+
