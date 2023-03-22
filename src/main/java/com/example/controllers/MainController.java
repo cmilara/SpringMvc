ec02e642b7ef4a27e78849e750fee5f1587f878f
cmilara <cristinamilara@gmail.com>
2023-03-22 10:49:31 +0100

    Telefonos insertados

--- src/main/java/com/example/controllers/MainController.java
+++ src/main/java/com/example/controllers/MainController.java
@@ -1,7 +1,9 @@
 package com.example.controllers;
 
 import java.lang.ProcessBuilder.Redirect;
+import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,18 +12,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 import com.example.entities.Estudiante;
 import com.example.entities.Facultad;
+import com.example.entities.Telefono;
 import com.example.services.EstudianteService;
 import com.example.services.FacultadService;
+import com.example.services.TelefonoService;
 
 
 @Controller
 @RequestMapping("/")
 public class MainController {
+
+    private static final Logger LOG = Logger.getLogger("MainController");
     
     @Autowired
     private EstudianteService estudianteService;
@@ -29,8 +36,8 @@ public class MainController {
     @Autowired
     private FacultadService facultadService;
 
-
-
+    @Autowired
+    private TelefonoService telefonoService;
 
 /*Un controlador responde a una peticion concreta y la delega en un método
  * que tiene en cuenta el verbo utilizado del protocolo HTTP para realizar la peticion
@@ -65,9 +72,34 @@ public class MainController {
  * Metodo que recibe los datos procedentes de los controles del formulario-ESTOY EN HOME-
  */
 @PostMapping("/altaEstudiante")
-public String altaEstudiante(@ModelAttribute Estudiante estudiante){
-    
+public String altaEstudiante(@ModelAttribute Estudiante estudiante, 
+    @RequestParam ("numerosTelefonos") String telefonosRecibidos ){
+
+        LOG.info("telefonos recibidos: " + telefonosRecibidos);
+
+    List<String>listadoNumerosTelefonos = null;
+
+         if(telefonosRecibidos != null){
+        String [] arrayTelefonos = telefonosRecibidos.split(";");
+
+       listadoNumerosTelefonos = Arrays.asList(arrayTelefonos);
+
+         }
+
     estudianteService.save(estudiante);
+
+    if(listadoNumerosTelefonos != null){
+        listadoNumerosTelefonos.stream().forEach(n -> {
+            Telefono telefonoObject = Telefono
+                    .builder()
+                    .numero(n)
+                    .estudiante(estudiante)
+                    .build();
+            telefonoService.save(telefonoObject);
+
+        });
+    }
+
     return "redirect:/listar";
 
     }
